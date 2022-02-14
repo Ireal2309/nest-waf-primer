4cd46c6512c2913d8c0b5073a8e705729b503c7c
Ireal2309 <92798378+Ireal2309@users.noreply.github.com>
2022-02-14 17:56:58 +0100

    Commit 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  getHome(): {} {
+    return {title: 'Home Page'};
+    }
+    getAboutUs(): {} {
+    return {title: 'About Us Page'};
   }
 }
+
